c42592855212a64bbc1b0bdd42df2a1e7277aa85
Martin Boyce <boycetrus@gmail.com>
2015-08-15 11:47:48 +1000

    events list
    added lists of featured events and all events to events page

--- js/components/incl-events.js
+++ js/components/incl-events.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-//hide recurring events from all events list 
+//hide recurring events from all events list
   $('<div class="toggle-events"><a id="toggle-events-btn">Hide Regular Events</a></div').appendTo('#all-events .dlv-search');
-    
-  //bind click event to button 
+
+  //bind click event to button
   $('#toggle-events-btn').on('click', function () {
     var recurringEvents = $('#all-events .js-eventPattern');
-      
+
   //adjust the btn text depending onthe visibility of recurring events
   if ( recurringEvents.is(':visible') ) {
       $('#toggle-events-btn').text('Show Regular Events');
     } else {
       $('#toggle-events-btn').text('Hide Regular Events');
     }
-    
+
   //on.click hide all storytimes from list
     recurringEvents.slideToggle('1200');
   });
