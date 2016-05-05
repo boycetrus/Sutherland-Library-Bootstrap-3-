58b3e2e4fdfcfc2a4926666eea7a913d77304e90
Martin Boyce <boycetrus@gmail.com>
2016-05-06 09:06:32 +1000

    added jQuery.cookie
    jQuery.cookie is used to set a cookie when the warning modal is shown
    so that it isn’t shown again upon form submit.

--- js/form-bootstrap.js
+++ js/form-bootstrap.js
@@ -1,11 +1,13 @@
+//@codekit-prepend "jquery.cookie.js";
+
 $(document).ready(function() {
-  $('.form-group label').addClass('control-label col-sm-3');
-  $('.form-group input').addClass('form-control');
-  $('.form-group select').addClass('form-control');
-  $('label > .se-form-required-text').addClass('sr-only');
+  $('.form-bootstrap .form-group label').addClass('control-label col-sm-3');
+  $('.form-bootstrap .form-group input').addClass('form-control');
+  $('.form-bootstrap .form-group select').addClass('form-control');
+  $('.form-bootstrap label > .se-form-required-text').addClass('sr-only');
   $('#ctl05_rpControls_ctl08_fiControl_se_form').attr('type','date');
-  $('.button-group input[type=submit]').addClass('btn btn-default hidden-print');
-  $('.form-error > a').addClass('text-danger');
+  $('.form-bootstrap .button-group input[type=submit]').addClass('btn btn-default hidden-print');
+  $('.form-bootstrap .form-error > a').addClass('text-danger');
 
    $('.form-error').each(function(i) {
     if ( $(this).children('a').length > 0) {
@@ -13,6 +15,9 @@ $(document).ready(function() {
     }
   });
 
-  $('#studentWarning').modal('show');
+  if ($.cookie('warning-modal') !== 'shown') {
+    $('#studentWarning').modal('show');
+    $.cookie('warning-modal', 'shown');
+  }
 
 });
