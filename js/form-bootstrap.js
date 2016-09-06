94f72e8906f9dc538ea68651c979e6007330a63b
Martin Boyce <boycetrus@gmail.com>
2016-09-06 10:16:56 +1000

    updates to student application form
    made a generic bootstrap form fixer js file and extended the student
    membership form js with a few extras

--- js/form-bootstrap.js
+++ js/form-bootstrap.js
@@ -1,15 +1,8 @@
-//@codekit-prepend "jquery.cookie.js";
-
 $(document).ready(function() {
   $('.form-bootstrap .form-group label').addClass('control-label col-sm-3');
   $('.form-bootstrap .form-group input').addClass('form-control');
   $('.form-bootstrap .form-group select').addClass('form-control');
   $('.form-bootstrap label > .se-form-required-text').addClass('sr-only');
-  $('#ctl05_rpControls_ctl06_fiControl_se_form').attr('type','email').attr('placeholder','eg. my.username@education.nsw.gov.au');
-  $('#ctl05_rpControls_ctl08_fiControl_se_form').attr('type','date');
-  $('#ctl05_rpControls_ctl12_fiControl_se_form').attr('disabled','disabled');
-  $('#ctl05_rpControls_ctl15_fiControl_se_form').attr('disabled','disabled');
-  $('#ctl05_rpControls_ctl18_fiControl_se_form').attr('disabled','disabled');
   $('.form-bootstrap .button-group input[type=submit]').addClass('btn btn-default hidden-print');
   $('.form-bootstrap .form-error > a').addClass('text-danger');
 
@@ -18,23 +11,5 @@ $(document).ready(function() {
       $(this).parents('.form-group').addClass('has-error');
     }
   });
-
-  if ($.cookie('warning-modal') !== 'shown') {
-    $('#studentWarning').modal('show');
-    $.cookie('warning-modal', 'shown');
-  }
-
-  $('#studentWarning').on ('hidden.bs.modal', function() {
-    $('#showWarning').removeClass('hidden');
-  });
-
-  $('#showWarning > .btn').on('click', function() {
-    $('#studentWarning').modal();
-  });
-
-  var $submitSuccess = $('#ctl05_divPostFormMessage');
-  if ($submitSuccess.length > 0) {
-    $.removeCookie('warning-modal');
-  }
 
 });
