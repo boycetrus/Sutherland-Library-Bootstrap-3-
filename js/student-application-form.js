0c3be77824fd9a26065fdab7e399c23270313490
Martin Boyce <boycetrus@gmail.com>
2016-09-06 14:09:57 +1000

    more student application form js refinements

--- js/student-application-form.js
+++ js/student-application-form.js
@@ -1,24 +1,12 @@
 //@codekit-prepend "jquery.cookie.js";
 
 $(document).ready(function() {
-  $('.form-bootstrap .form-group label').addClass('control-label col-sm-3');
-  $('.form-bootstrap .form-group input').addClass('form-control');
-  $('.form-bootstrap .form-group select').addClass('form-control');
-  $('.form-bootstrap label > .se-form-required-text').addClass('sr-only');
   $('#ctl05_rpControls_ctl06_fiControl_se_form').attr('type','email').attr('placeholder','eg. my.username@education.nsw.gov.au');
   $('#ctl05_rpControls_ctl08_fiControl_se_form').attr('type','date');
   $('#ctl05_rpControls_ctl15_fiControl_se_form').attr('disabled','disabled');
   $('#ctl05_rpControls_ctl18_fiControl_se_form').attr('disabled','disabled');
   $('#ctl05_rpControls_ctl21_fiControl_se_form').attr('disabled','disabled');
-  $('.form-bootstrap .button-group input[type=submit]').addClass('btn btn-default');
   $('<a id="printBtn" class="btn btn-primary" href="javascript:window.print();" role="button">Print</a> ').prependTo('.form-bootstrap .button-group');
-  $('.form-bootstrap .form-error > a').addClass('text-danger');
-
-   $('.form-error').each(function(i) {
-    if ( $(this).children('a').length > 0) {
-      $(this).parents('.form-group').addClass('has-error');
-    }
-  });
 
   if ($.cookie('warning-modal') !== 'shown') {
     $('#studentWarning').modal('show');
