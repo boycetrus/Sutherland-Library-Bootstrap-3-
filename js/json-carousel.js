c81e7eaee54b27d9ab520ca73d1191ababa53a0c
Martin Boyce <boycetrus@gmail.com>
2017-01-27 13:58:34 +1100

    book carousel using json data minus styling

--- js/json-carousel.js
+++ js/json-carousel.js
@@ -5,24 +5,44 @@ $('document').ready(function() {
 
   //fetch the json feed
   $.getJSON( jsonSource, function() {
-    console.log("json request successful");
+    console.log("json request success");
   })
   .done(function(data) {
     if (data.feed.entry.length === 0) {
       //if the json request is successful but there are no items
       $("<p class='text-danger'>JSON request succeeded but no data returned.</p>").prependTo($carousel);
     } else {
-     // Handlebars compiles the template into a callable function
-     var template = $("#bookTemplate").html();
-     // call the compiled function with the template data
-     var renderer = Handlebars.compile(template);
-     //data from the ajax call as an array
-     var context = data;
-     //wrap the context array in an object and create html
-     //this is the bit that goes wrong depending on whether the json is an object or an array
-     var result = renderer({data:context});
-     //populate the #book-list with the resulting html
-     $("#book-list").html(result);
+      console.log(data);
+      // Handlebars compiles the template into a callable function
+      var template = $("#bookTemplate").html();
+      // call the compiled function with the template data
+      var renderer = Handlebars.compile(template);
+      //data from the ajax call as an array
+      var context = data;
+      //wrap the context array in an object and create html
+      //this is the bit that goes wrong depending on whether the json is an object or an array
+      var result = renderer({data:context});
+
+      //populate the #book-list with the resulting html
+      $("#bookCarousel").html(result);
+
+      //generate the carousel
+      $carousel.owlCarousel({
+        margin:10,
+        nav:true,
+        navText: ['<i data-icon="&#xe027;"></i>','<i data-icon="&#xe025;"></i>'],
+        dots: false,
+        responsive:{
+          0   : {items:1},
+          400 : {items:2},
+          600 : {items:3},
+          800 : {items:4},
+          1000: {items:5},
+          1200: {items:6},
+          1400: {items:7},
+          1600: {items:8}
+        }
+      });
     }
   })
   .fail(function() {
@@ -32,19 +52,4 @@ $('document').ready(function() {
     console.log("JSON request error");
   });
 
-  $carousel.owlCarousel({
-    margin:10,
-    nav:true,
-    dots: true,
-    responsive:{
-      0   : {items:1},
-      400 : {items:2},
-      600 : {items:3},
-      800 : {items:4},
-      1000: {items:5},
-      1200: {items:6},
-      1400: {items:7},
-      1600: {items:8}
-    }
-  });
 });
