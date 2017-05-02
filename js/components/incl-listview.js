6e6d690bea5984e4b8995a499085d5a6981f140e
Martin Boyce <boycetrus@gmail.com>
2017-05-02 16:23:36 +1000

    pagination for blog

--- js/components/incl-listview.js
+++ js/components/incl-listview.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-//  $('.dlv-thumbnails .thumbnails .span3:nth-of-type(4n+1)').addClass('new-row');
-//  $('.dlv-thumbnails .thumbnails .span4:nth-of-type(3n+1)').addClass('new-row');
-//  $('.dlv-thumbnails .thumbnails .span6:nth-of-type(2n+1)').addClass('new-row');
+
 
 });
