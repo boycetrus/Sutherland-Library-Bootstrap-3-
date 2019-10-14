aaab1e209154ae49bf9136ba038c3263f06caa35
Martin Boyce <boycetrus@gmail.com>
2019-10-14 13:50:21 +1100

    change variable names and adjust center lat:lng

--- js/blog-map.js
+++ js/blog-map.js
@@ -1,4 +1,4 @@
-var stones = [
+var milestones = [
   ['Sydney 13 Miles, Pt Hacking 5 Miles', -34.009193, 151.109792, 1],
   ['Sydney 15 Miles, Pt Hacking 3 Miles', -34.043338, 151.123387, 2],
   ['Sydney 17 Miles, Pt Hacking 1 Miles', -34.029806, 151.059510, 3],
@@ -6,23 +6,22 @@ var stones = [
 ];
 
 function setMarkers(map) {
-  for (var i = 0; i < stones.length; i++) {
-    var stone = stones[i];
+  for (var i = 0; i < milestones.length; i++) {
+    var milestone = milestones[i];
     var marker = new google.maps.Marker({
-      position: {lat: stone[1], lng: stone[2]},
+      position: {lat: milestone[1], lng: milestone[2]},
       map: map,
-      title: stone[0],
-      zIndex: stone[3]
+      title: milestone[0],
+      zIndex: milestone[3]
     });
   }
 }
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
+    zoom: 13,
     map: map,
-    mapTypeID: roadmap,
-    center: {lat: -34.037849, lng: 151.084453}
+    center: {lat: -34.035218, lng: 151.093630}
   });
 
   setMarkers(map);
