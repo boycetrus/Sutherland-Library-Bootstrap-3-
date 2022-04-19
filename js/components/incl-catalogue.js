69e1ebe10eda0d152f20ea31502ad4386253a860
Martin Boyce <boycetrus@gmail.com>
2022-04-20 09:37:00 +1000

    header search redirected to Vega
    updated the search form in the site template to search Vega instead of Encore

--- js/components/incl-catalogue.js
+++ js/components/incl-catalogue.js
@@ -1,19 +1,19 @@
 function searchCatalogue() {
-  var baseUrl = "https://encore.sutherlandshire.nsw.gov.au/iii/encore/search/C__S";
+  var baseUrl = "https://suth.ap.iiivega.com/search?searchType=everything&pageSize=10&query=";
   var searchTerm = $('#catalog-search-textbox').val();
   var destination = baseUrl + searchTerm;
   //when catalogue search is submitted fetch the new url
   window.location.href = destination;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('#catalog-search-btn').on('click', function(event) {
+  $('#catalog-search-btn').on('click', function (event) {
     event.preventDefault();
     searchCatalogue();
   });
 
-  $('#catalog-search-textbox').keydown(function(event){
+  $('#catalog-search-textbox').keydown(function (event) {
     if (event.which === 13) {
       event.preventDefault();
       searchCatalogue();
